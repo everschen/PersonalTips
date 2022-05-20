12bdeb04fd506a700a833b4df4c7a6fb2f773bd3
Evers Chen <evers_chen@163.com>
2022-05-20 10:01:58 +0800

    add file

---
+++ bat/mystart.bat
@@ -0,0 +1,33 @@
+::start chrome.exe https://github.com/everschen/Alchemy/blob/master/pyg/mpnn.py
+::start chrome.exe https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/nn/conv/nn_conv.html#NNConv.forward
+::start chrome.exe https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/nn/conv/message_passing.html#MessagePassing
+
+::start chrome.exe https://github.com/tencent-alchemy/Alchemy/blob/master/dgl/train.py
+::start chrome.exe https://github.com/tencent-alchemy/Alchemy/blob/master/dgl/mgcn.py
+::start chrome.exe https://github.com/tencent-alchemy/Alchemy/blob/master/dgl/Alchemy_dataset.py
+
+::start chrome.exe https://tianchi.aliyun.com/competition/gameList/activeList
+start chrome.exe http://www.sohu.com/
+
+::start chrome.exe https://github.com/everschen/Alchemy/blob/master/pyg/Alchemy_dataset.py
+
+::start chrome.exe https://github.com/everschen/models/tree/develop/PaddleCV/yolov3
+
+::start chrome.exe https://dsw-cn-beijing.data.aliyun.com/notebook.htm?instance=3705#/
+::start chrome.exe https://github.com/everschen/models/blob/develop/PaddleCV/PaddleDetection/tools/train.py
+::start chrome.exe https://pai.data.aliyun.com/console?regionId=cn-beijing#/notebook
+
+start "" "C:\Program Files (x86)\Evernote\Evernote\Evernote.exe"
+
+start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+
+start "" "C:\Users\Administrator\AppData\Local\Kingsoft\Power Word 2016\2016.3.3.0349\PowerWord.exe"
+
+start "" "C:\Program Files\Notepad++\notepad++.exe"
+
+start winword "F:\??\???????.docx"
+
+::start "" "C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe" -config c:\\192.168.31.188.vnc
+
+::start "" "C:\Program Files (x86)\Source Insight 3\Insight3.exe"
+::start "" "E:\Atom x64\atom.exe"
